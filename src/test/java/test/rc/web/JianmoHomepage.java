a6c935f898c1e18f312d328618da438a2edd06c5
Administrator <Administrator@172.16.201.129>
2015-10-22 15:56:34 +0800

    1.add Homepage Testing class 2.update base url

---
+++ src/test/java/test/rc/web/JianmoHomepage.java
@@ -0,0 +1,21 @@
+package test.rc.web;
+
+import org.junit.Test;
+
+import product.BaseTest;
+
+public class JianmoHomepage extends BaseTest {
+
+	@Test
+	public void testJianmoHomepage() throws Exception {
+		selenium.open("/");
+		selenium.click("link=更多课程");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=EDUSOHO测试站");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=最热");
+		selenium.click("link=推荐");
+		selenium.click("link=最新");
+	}
+
+}
