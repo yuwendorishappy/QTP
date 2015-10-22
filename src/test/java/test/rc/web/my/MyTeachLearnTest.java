f74ea93523b707f970fc3e3bc94b07c2dccf3883
Administrator <Administrator@172.16.201.129>
2015-10-22 16:59:22 +0800

    add my teaching and learning menu testing

---
+++ src/test/java/test/rc/web/my/MyTeachLearnTest.java
@@ -0,0 +1,84 @@
+package test.rc.web.my;
+
+import org.junit.Test;
+
+import product.BaseTest;
+
+public class MyTeachLearnTest extends BaseTest {
+
+	@Test
+	public void testMyTeachLearn() throws Exception {
+		selenium.open("/");
+		selenium.click("css=li.hidden-xs > a");
+		selenium.waitForPageToLoad("30000");
+		selenium.type("id=login_username", "test@edusoho.com");
+		selenium.type("id=login_password", "kaifazhe");
+		selenium.click("//button[@type='submit']");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("css=li.visible-lg > a");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=班级课程");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=学员问答");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=学员话题");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=试卷批阅");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的课程");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的问答");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的话题");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的笔记");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的考试");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的小组");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=加入的小组");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=发起的话题");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=回复的话题");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=收藏的话题");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的考试");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=收藏的题目");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的笔记");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的话题");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=班级话题");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的问答");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=我的课程");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=已学完");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=收藏");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=试卷批阅");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=已批阅");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=学员话题");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=班级学员话题");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=学员问答");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=在教课程");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=班级课程");
+		selenium.waitForPageToLoad("30000");
+		selenium.click("link=退出登录");
+		selenium.waitForPageToLoad("30000");
+	}
+
+}
