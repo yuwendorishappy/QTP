ffbe4a316e7f7e7e7b6aae55968e53057d320465
Stanley <sunminghui@howzhi.com>
2015-10-28 16:27:17 +0800

    add view course

---
+++ src/test/java/test/rc/web/course/ViewCouseTest.java
@@ -0,0 +1,13 @@
+package test.rc.web.course;
+
+import org.junit.Test;
+
+import product.BaseTest;
+
+public class ViewCouseTest extends BaseTest {
+	
+	@Test
+	public void testMyTeachLearn() throws Exception {
+		selenium.open("/");
+	}
+}
