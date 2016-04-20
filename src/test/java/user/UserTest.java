e6a17effcd33d976be651889a365beb96857f4bb
jingxueying <jingxueying@howzhi.com>
2016-04-20 18:18:10 +0800

    test

--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -22,7 +22,7 @@ public class UserTest {
 	  driver = new ChromeDriver();
 	  driver.navigate().to(url);
 	  Assert.assertEquals("http://t5.edusoho.cn", url);
-      System.out.println("..........œµÕ≥URL+url+................");
+      System.out.println("..........系统URL+url+................");
 	  driver.manage().window().maximize();
      
    }
@@ -37,20 +37,20 @@ public class UserTest {
 		 username=driver.findElement(By.id("login_username"));    
 		 username.sendKeys(telephonename);
 		 
-		//—È÷§
+	
 		 Assert.assertEquals("13757160849", telephonename);
-		 System.out.println("..............'telephonename'+'∂œ—‘OK'+.................");
+		 System.out.println("..............'telephonename'+‘OK'+.................");
 		 
 		 password=driver.findElement(By.id("login_password"));
 		 password.sendKeys(telephonepwd);
 		 
-		//—È÷§
+		
 		 Assert.assertEquals("123456789", telephonepwd);
-		 System.out.println("..............'telephonepwd'+'∂œ—‘OK'+.................");
+		 System.out.println("..............'telephonepwd'+'‘OK'+.................");
 		 remember=driver.findElement(By.name("_remember_me"));
 		 remember.click();
 		 Assert.assertTrue(remember.isEnabled());
-		 System.out.println("º«◊°√‹¬Îπ¶ƒ‹OK");
+		 System.out.println("‹OK");
 		 loginbutton=driver.findElement(By.xpath("//*[@id='login-form']/div[4]/button"));
 		 loginbutton.click();
 		 driver.equals(driver.getCurrentUrl());
@@ -59,22 +59,22 @@ public class UserTest {
   @Parameters({"youxiangname","youxiangpwd"})
   @Test
   public void youxianglogin(String youxiangname,String youxiangpwd) {
-	 login = driver.findElement(By.linkText("µ«¬º"));
+	 login = driver.findElement(By.linkText("登录"));
 	 login.click();
 	 loginpage=driver.findElement(By.xpath("//*[@id='content-container']/div/div[1]/a[1]"));
 	 loginpage.click();
 	 username=driver.findElement(By.id("login_username"));    
 	 username.sendKeys(youxiangname);
-	 //—È÷§
+	 
 	 Assert.assertEquals("286031476@qq.com", youxiangname);
-	 System.out.println("..............'youxiangname'+'∂œ—‘OK'+.................");
+	 System.out.println("..............'youxiangname'+'‘OK'+.................");
 	 
 	 password=driver.findElement(By.id("login_password"));
 	 password.sendKeys(youxiangpwd);
 	 
-	 //—È÷§
+	 
 	 Assert.assertEquals("123456789", youxiangpwd);
-	 System.out.println("..............'youxiangpwd'+'∂œ—‘OK'+.................");
+	 System.out.println("..............'youxiangpwd'+'‘OK'+.................");
 	 
 	 remember=driver.findElement(By.name("_remember_me"));
 	 remember.click();
@@ -88,28 +88,28 @@ public class UserTest {
   @Parameters({"userloginname","userloginpwd"})
   @Test
   public void usernamelogin(String userloginname,String userloginpwd) {
-	 login = driver.findElement(By.linkText("µ«¬º"));
+	 login = driver.findElement(By.linkText("登录"));
 	 login.click();
 	 loginpage=driver.findElement(By.xpath("//*[@id='content-container']/div/div[1]/a[1]"));
 	 loginpage.click();
 	 username=driver.findElement(By.id("login_username"));    
 	 username.sendKeys(userloginname);
-	 //—È÷§
+	
 	 Assert.assertEquals("lisi", userloginname);
-	 System.out.println("..............'userloginname'+'∂œ—‘OK'+.................");
+	 System.out.println("..............'userloginname'+'‘OK'+.................");
 	 
 	 password=driver.findElement(By.id("login_password"));
 	 password.sendKeys(userloginpwd);
 	 
 	 Assert.assertEquals("123456789", userloginpwd);
-	 System.out.println("..............'userloginpwd'+'∂œ—‘OK'+.................");
+	 System.out.println("..............'userloginpwd'+'‘OK'+.................");
 	 
 	 remember=driver.findElement(By.name("_remember_me"));
 	 remember.click();
 	 loginbutton=driver.findElement(By.xpath("//*[@id='login-form']/div[4]/button"));
 	 loginbutton.click();
 	 driver.equals(driver.getCurrentUrl());
-	 }
+}
   @AfterMethod
 	 public void aftermethod() {
 		  driver.close();
