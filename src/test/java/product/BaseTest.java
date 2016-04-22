5a9e5b59d3c179b108f2390e84518138a1b367e1
baolianjing <baolianjing@howzhi.com>
2016-04-22 12:10:46 +0800

    test rebuild

--- src/test/java/product/BaseTest.java
+++ src/test/java/product/BaseTest.java
@@ -4,16 +4,14 @@ import java.io.InputStream;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.AfterClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 
-import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.Selenium;
 
 public class BaseTest {
@@ -23,7 +21,8 @@ public class BaseTest {
 	protected static WebDriver driver = null;
 	protected static String baseUrl;
 
-	static {
+	@BeforeClass
+	public void beforeClass(){
 		InputStream in = BaseTest.class.getResourceAsStream("/selenium.properties");
 		try {
 			prop = new Properties();
@@ -33,56 +32,29 @@ public class BaseTest {
 		}
 	}
 
-	@BeforeClass
-	public static void beforeClass() throws Exception {
-
-		String seleniumRunMode = prop.getProperty("seleniumRunMode");
-		baseUrl = BaseTest.prop.getProperty("baseUrl");
-
-		if ("webDriver".equals(seleniumRunMode)) {
-
-			String browser = prop.getProperty("browser");
-			if ("chrome".equals(browser)) {
-
-				System.setProperty("webdriver.chrome.driver",
-						prop.getProperty("selenium.webDriver.File"));
-				driver = new ChromeDriver();
-			} else if ("firefox".equals(browser)) {
-
-				System.setProperty("webdriver.firefox.bin",
-						prop.getProperty("selenium.webDriver.File"));
-				driver = new FirefoxDriver();
-			} else if ("iexplore".equals(browser)) {
-
-				System.setProperty("webdriver.ie.driver",
-						prop.getProperty("selenium.webDriver.File"));
-
-				driver = new InternetExplorerDriver();
-
-			}
-			driver.manage().window().maximize();
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
-		} else if ("remoteControl".equals(seleniumRunMode)) {
-			selenium = new DefaultSelenium(prop.getProperty("rc.url"),
-					Integer.valueOf(prop.getProperty("rc.port")), "*"
-							+ prop.getProperty("browser"),
-					prop.getProperty("test.url"));
-			selenium.start();
-			selenium.windowMaximize();
-
+	@BeforeMethod
+	public void beforeMethod() {
+		baseUrl = BaseTest.prop.getProperty("test.url");
+		String browser = prop.getProperty("browser");
+		if ("chrome".equals(browser)) {
+			System.setProperty("webdriver.chrome.driver", prop.getProperty("selenium.webDriver"));
+			driver = new ChromeDriver();
+		} else if ("firefox".equals(browser)) {
+			System.setProperty("webdriver.firefox.bin", prop.getProperty("selenium.webDriver"));
+			driver = new FirefoxDriver();
+		} else if ("iexplore".equals(browser)) {
+			System.setProperty("webdriver.ie.driver", prop.getProperty("selenium.webDriver"));
+			driver = new InternetExplorerDriver();
 		}
+		driver.navigate().to(baseUrl);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
 
-	@AfterClass
-	public static void afterClass() {
-		String seleniumDriver = prop.getProperty("seleniumRunMode");
-		if ("webDriver".equals(seleniumDriver)) {
-			driver.close();
-			driver.quit();
-		} else if ("remoteControl".equals(seleniumDriver)) {
-			selenium.stop();
-		}
+	@AfterMethod
+	public void aftermethod() {
+		driver.close();
+		driver.quit();
 
 	}
 
